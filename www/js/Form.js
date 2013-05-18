48bb0c576e8fd3da17e6a68dadb6f8abd1b1bbfd
Mukhin Alexey <boogerm2@mail.ru>
2013-05-19 04:56:03 +1100

    Теперь форму можно перемещать

--- www/js/Form.js
+++ www/js/Form.js
@@ -12,7 +12,7 @@ function FormSubmit(event){
 }
 
 function Form(selector){
-    this.form = $(selector).submit()
+    this.form = $(selector).submit().draggable()
     this.form.zIndex(100)
     this.form.submit(FormSubmit)
     this.test = $(selector + ' input.cancel').click({form: this}, FormCancel)
@@ -69,71 +69,6 @@ AddForm.prototype.hide = function(){
     this.add_button.fadeIn()
 }
 
-/*
-var oldid = -1
-var af
-var uf
-var ab
-var form_size = 0
-function show_add_form(){
-    af.style.display = 'block'
-    hide_form(uf)
-    ab.style.display = 'none'
-    form_size = af.offsetHeight
-}
-
-function hide_form(form){
-    form.style.display = 'none'
-    if (af.style.display == 'none' && uf.style.display == 'none')
-        ab.style.display = 'block'
-}
-
-function fill(data){
-
-}
-
-function show_update_form(args){
-    hide_form(af)
-    ab.style.display = 'none'
-    for (var i = 0; i < args.length; ++i){
-        if (args[i] instanceof Array){
-            for(var j = 0; j < uf[i].options.length; ++j){
-                uf[i].options[j].selected = false
-                if (args[i].indexOf(parseInt(uf[i].options[j].value)) != -1)
-                    uf[i].options[j].selected = true
-            }
-        }else
-            uf[i].value = args[i]
-    }
-    var newid = args[0]
-    if (oldid == newid){
-        hide_form(uf)
-        oldid = -1
-    } else {
-        uf.style.display =  'block'
-        oldid = newid
-    }
-    form_size = uf.offsetHeight
-}
-
-function message(caption, color){
-    $('#message').text(caption)
-    var top = form_size + 25
-    $('#message').css({top: top})
-    $('#message').css('display','block')
-    $('#message').css('color',color)
-    $('#message').fadeOut(2000)
-}
-
-function check_form(){
-    for(var i = 0; i < this.elements.length; ++i)
-        if(this[i].value == ''){
-            message('Заполните все поля!', '#FF0000')
-            return false
-        }
-}
- */
-
 function NumFilter(event){
     if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
        ( event.keyCode >= 35 && event.keyCode <= 39) || ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )))
@@ -148,5 +83,6 @@ function PrepareForms(){
     SetNumFilters()
     addForm = new AddForm('form#add', 'button#add_button')
     updateForm = new Form('form#update')
+
     //addbutton =
 }
